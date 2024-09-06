9c45332017fd7e7e6e54a680e3185fa47e220c4b
BOTI <>
2024-09-06 09:12:41 +0200

    aha

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace C_First;
+
+class Program
+{
+    private float szam=1.5f;
+    private float szam2=3.2f;
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Az első argumrntum: "+ args[0]);
+
+    }
+}
